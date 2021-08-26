44b6a815f92d01b20ac49ca0aa2f132074a506cd
sskcvqikf <23529877+sskcvqikf@users.noreply.github.com>
2021-08-26 16:53:00 +0300

    Hello, world! works

--- brainfuck.cc
+++ brainfuck.cc
@@ -2,19 +2,21 @@
 #include <memory>
 #include <exception>
 #include <algorithm>
+#include <stack>
 
 using byte_t = char;
 static constexpr int size = 30000;
 
 namespace detail
 {
+struct brainfuck_tree;
 
 struct op
 {
     op(std::shared_ptr<int> dp, byte_t *data_ptr)
         : dp_(std::move(dp)), data_ptr_(data_ptr) {}
 
-    void execute()
+    virtual void execute()
     {
         execute_impl();
         if (nop_.get() != nullptr)
@@ -26,15 +28,26 @@ struct op
         nop_ = std::move(nop);
         return  nop_.get();
     }
-    
-    std::shared_ptr<int> get_dp() { return dp_; }
+
     byte_t* get_data_ptr() { return data_ptr_; }
+    op* get_nop_() { return nop_.get(); }
+    std::shared_ptr<int> get_dp() { return dp_; }
+    
 private:
     virtual void execute_impl() = 0;
 
+protected:
     std::unique_ptr<op> nop_; // next instruction
     std::shared_ptr<int> dp_; // data ptr
     byte_t *data_ptr_;
+};
+
+struct brainfuck_tree : op
+{
+    using op::op;
+    void prepare() {}
+private:
+    void execute_impl(){}
 };
 
 struct idp : op // increment data pointer
@@ -44,7 +57,7 @@ struct idp : op // increment data pointer
 private:
     void execute_impl() override
     {
-        if (++*get_dp() >= (size - 1))
+        if (++*dp_ >= (size - 1))
             throw std::overflow_error("Data pointer is more than array size");
     }
 };
@@ -56,7 +69,7 @@ struct ddp : op // decrement data pointer
 private:
     void execute_impl() override
     {
-        if(--*get_dp() < 0)
+        if(--*dp_ < 0)
             throw std::overflow_error("Data pointer is less than 0");
     }
 };
@@ -68,7 +81,7 @@ struct id : op // increment byte in current cell
 private:
     void execute_impl() override
     {
-        ++(*(get_data_ptr() + *get_dp()));
+        ++*(data_ptr_ + *dp_);
     }
 };
 
@@ -79,7 +92,7 @@ struct dd : op // decrement byte in current cell
 private:
     void execute_impl() override
     {
-        --(*(get_data_ptr() + *get_dp()));
+        --*(data_ptr_ + *dp_);
     }
 };
 
@@ -90,26 +103,63 @@ struct od : op // output byte in current cell
 private:
     void execute_impl() override
     {
-        std::cout << (*(get_data_ptr() + *get_dp()));
+        std::cout << *(data_ptr_ + *dp_);
     }
-};
-
-struct bl : op // begin loop
-{
-    using op::op;
 };
 
 struct el : op // end loop
 {
-    using op::op;
+    using base = op;
+    
+    el(std::shared_ptr<int> dp, byte_t *data_ptr, op *bl_ptr)
+        : base(dp, data_ptr), bl_ptr_(bl_ptr) {}
+
+    void execute() override
+    {
+        if (*(data_ptr_ + *dp_) == 0)
+        {
+            if (nop_ != nullptr)
+                nop_->execute();
+        }
+        else
+            bl_ptr_->execute();
+    }
+
+private:
+    void execute_impl() override {}
+    op* bl_ptr_ = nullptr;
 };
 
-struct brainfuck_tree : op
+struct bl : op // begin loop
 {
-    using op::op;
-    void prepare() {}
+    using base = op;
+
+    bl(std::shared_ptr<int> dp, byte_t *data_ptr)
+        : base(dp, data_ptr), tree_(dp, data_ptr), last_(&tree_) {}
+
+    op* add_next(std::unique_ptr<op> nop) override
+    {
+        last_ = last_->add_next(std::move(nop));
+        return last_;
+    }
+
+
+    void execute() override
+    {
+        if (*(data_ptr_ + *dp_) == 0)
+        {
+            if (nop_ != nullptr)
+                nop_->execute();
+        }
+        else
+            tree_.execute();
+    }
+
 private:
-    void execute_impl(){}
+    void execute_impl() override {}
+    bool is_completed_{false};
+    brainfuck_tree tree_;
+    op* last_;
 };
 
 } // namespace detail
@@ -120,6 +170,7 @@ struct brainfuck
     brainfuck(const char* bf)
         : tree_(std::make_shared<int>(0), data_), last_(&tree_)
     {
+        std::stack<detail::op*> stack_;
         for(const char* i = bf; *i != '\0'; ++i)
         {
             char di = *i;
@@ -145,6 +196,24 @@ struct brainfuck
                     last_ = last_->add_next(std::make_unique<detail::od>(last_->get_dp(),
                         last_->get_data_ptr()));
                     break;
+                case '[':
+                    last_ = last_->add_next(std::make_unique<detail::bl>(last_->get_dp(),
+                        last_->get_data_ptr()));
+                    stack_.push(last_);
+                    break;
+                case ']':
+                    last_ = last_->add_next(std::make_unique<detail::el>(last_->get_dp(),
+                        last_->get_data_ptr(), stack_.top()));
+                    stack_.pop();
+                    break;
+                case '\n':
+                    break;
+                case '\t':
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw std::invalid_argument("Unknown symbol appeared");
             }
         }
     }
