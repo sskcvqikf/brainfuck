e509c3ab83651cdabce8949745b0bd837f422293
sskcvqikf <23529877+sskcvqikf@users.noreply.github.com>
2021-08-30 13:29:52 +0300

    factory for creating different operations created

--- brainfuck.cc
+++ brainfuck.cc
@@ -42,20 +42,22 @@ struct op_simple : op
     virtual void execute()
     {
         execute_impl();
-        if (nop_.get() != nullptr)
-            nop_.get()->execute();
+        if (nop_)
+            nop_->execute();
     }
 private:
     virtual void execute_impl() = 0;
 };
 
 
-struct brainfuck_tree final : op_simple 
+struct brainfuck_tree final : op
 {
-    using op_simple::op_simple;
-    void prepare() {}
-private:
-    void execute_impl() override{}
+    using op::op;
+    void execute() override
+    {
+        if (nop_)
+            nop_->execute();
+    }
 };
 
 struct idp final : op_simple // increment data pointer
@@ -82,7 +84,7 @@ private:
     }
 };
 
-struct id final : op_simple // increment byte in current cell
+struct ib final : op_simple // increment byte in current cell
 {
     using op_simple::op_simple;
 
@@ -93,7 +95,7 @@ private:
     }
 };
 
-struct dd final : op_simple // decrement byte in current cell
+struct db final : op_simple // decrement byte in current cell
 {
     using op_simple::op_simple;
 
@@ -104,7 +106,7 @@ private:
     }
 };
 
-struct od final : op_simple // output byte in current cell
+struct ob final : op_simple // output byte in current cell
 {
     using op_simple::op_simple;
 
@@ -157,6 +159,52 @@ private:
     op* el_ptr_;
 };
 
+struct op_factory final
+{
+    op_factory(std::shared_ptr<int> dp, char *data)
+        : dp_(std::move(dp)), data_(data) {}
+
+    std::unique_ptr<op> get_idp() { return std::make_unique<idp>(dp_, data_); }
+
+    std::unique_ptr<op> get_ddp() { return std::make_unique<ddp>(dp_, data_); }
+    
+    std::unique_ptr<op> get_ib() { return std::make_unique<ib>(dp_, data_); }
+
+    std::unique_ptr<op> get_db() { return std::make_unique<db>(dp_, data_); }
+
+    std::unique_ptr<op> get_ob() { return std::make_unique<ob>(dp_, data_); }
+
+    std::unique_ptr<op> get_bl()
+    { 
+        auto el_ptr = std::make_unique<detail::el>(dp_, data_);
+        auto rtn = std::make_unique<detail::bl>(dp_, data_, el_ptr.get());
+        el_ptr->set_bl_ptr(rtn.get());
+        stack_el.push(std::move(el_ptr));
+        return rtn;
+    }
+
+    std::unique_ptr<op> get_el()
+    {
+        if (stack_el.empty())
+            throw std::invalid_argument("Bad brainfuck string");
+        auto rtn(std::move(stack_el.top()));
+        stack_el.pop();
+
+        return rtn;
+    }
+
+    void post_process()
+    {
+        if (!stack_el.empty())
+            throw std::invalid_argument("Bad brainfuck string");
+    }
+
+private:
+    std::stack<std::unique_ptr<el>> stack_el;
+    std::shared_ptr<int> dp_;
+    char* data_;
+};
+
 } // namespace detail
 
 
@@ -165,47 +213,36 @@ struct brainfuck final
     brainfuck(const char* bf)
         : tree_(std::make_shared<int>(0), data_), last_(&tree_)
     {
-        std::stack<std::unique_ptr<detail::el>> stack_el;
+        detail::op_factory factory(std::move(tree_.get_dp()), tree_.get_data_ptr());
+
         for(const char* i = bf; *i != '\0'; ++i)
         {
             char di = *i;
             switch (di)
             {
                 case '>':
-                    last_ = last_->add_next(std::make_unique<detail::idp>(tree_.get_dp(),
-                        tree_.get_data_ptr()));
+                    last_ = last_->add_next(factory.get_idp());
                     break;
                 case '<':
-                    last_ = last_->add_next(std::make_unique<detail::ddp>(tree_.get_dp(),
-                        tree_.get_data_ptr()));
+                    last_ = last_->add_next(factory.get_ddp());
                     break;
                 case '+':
-                    last_ = last_->add_next(std::make_unique<detail::id>(tree_.get_dp(),
-                        tree_.get_data_ptr()));
+                    last_ = last_->add_next(factory.get_ib());
                     break;
                 case '-':
-                    last_ = last_->add_next(std::make_unique<detail::dd>(tree_.get_dp(),
-                        tree_.get_data_ptr()));
+                    last_ = last_->add_next(factory.get_db());
                     break;
                 case '.':
-                    last_ = last_->add_next(std::make_unique<detail::od>(tree_.get_dp(),
-                        tree_.get_data_ptr()));
+                    last_ = last_->add_next(factory.get_ob());
                     break;
                 case '[':
                 {
-                    auto el_ptr = std::make_unique<detail::el>(tree_.get_dp(), tree_.get_data_ptr());
-                    last_ = last_->add_next(std::make_unique<detail::bl>(last_->get_dp(),
-                        last_->get_data_ptr(), el_ptr.get()));
-                    el_ptr->set_bl_ptr(last_);
-                    stack_el.push(std::move(el_ptr));
+                    last_ = last_->add_next(factory.get_bl());
                     break;
                 }
                 case ']':
                 {
-                    if (stack_el.empty())
-                        throw std::invalid_argument("Bad brainfuck string");
-                    last_ = last_->add_next(std::move(stack_el.top()));
-                    stack_el.pop();
+                    last_ = last_->add_next(factory.get_el());
                     break;
                 }
                 case '\n':
@@ -218,8 +255,7 @@ struct brainfuck final
                     throw std::invalid_argument("Unknown symbol appeared");
             }
         }
-        if (!stack_el.empty())
-            throw std::invalid_argument("Bad brainfuck string");
+        factory.post_process();
     }
 
     void execute()
